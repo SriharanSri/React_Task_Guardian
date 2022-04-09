2a136a142deb5dda6d88f70dcae5866cacdfa7e4
SriharanSri <92523322+SriharanSri@users.noreply.github.com>
2022-04-09 15:42:46 +0530

    ...

--- src/components/Discard.js
+++ src/components/Discard.js
@@ -10,7 +10,7 @@ function Discard() {
 
   React.useEffect(() => {
     // getdata();
-    // getCommentdata();
+    getCommentdata();
     dispatch(fetchUserData())
     dispatch(fetchUserTodo());
     
